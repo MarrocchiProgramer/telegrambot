ff581bfeecfcf0b3c57cb0db519332269ffa458e
Tomas Marrocchi <marrocchitomaset36@gmail.com>
2022-10-15 20:12:27 -0300

    bot andando

--- bot.py
+++ bot.py
@@ -2,40 +2,68 @@ import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
-def process_message(update, context):
-    text = update.message.text
 
-    if str(text).__contains__('#channel'):
+
+def start (update, context):
+
+    mensaje=update.message.reply_text(
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Go to the bot', url='https://youtube.com')]
+        ])
+    )
+    
+    if update.message.text.__contains__('hola'):
         context.bot.send_message(
-            chat_id='-1001734611459',
-            text=str(text).replace('#channel', '')
+           chat_id='-1001734611459',
+           text=mensaje
         )
 
-    print(str(text))
+  
 
 
 if __name__ == '__main__':
-    updater = Updater(token='5772798648:AAELl54LNmwV2bg12CR8ugiyTDln_l2aLsE', use_context=True)   
+    updater = Updater(token='5772798648:AAELl54LNmwV2bg12CR8ugiyTDln_l2aLsE', use_context=True)
 
-    dp = updater.dispatcher   
-    dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))     
+    dp = updater.dispatcher
+    dp.add_handler(
+        CommandHandler('start', start)
+    )
 
-    updater.start_polling()
+    updater.start_polling() 
     print('El bot se inicio')
     updater.idle()
+    
 
-# https://t.me/+P1X0RU5CLjliMDhh
+
+
+
+
+
+#PARA MANDAR COSAS 
 # dp.add_handler(CommandHandler('start', start))
+# dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))   
 
-# def start (update, context):
-#     update.message.reply_text('Hola como estas')
-#     boton1 = InlineKeyboardButton( 
-#         text='Que dia faltara',
-#         url='https://youtube.com'
-#     )
-#     update.message.reply_text(
-#         text='Hace click en el boton',
-#         reply_markup=InlineKeyboardMarkup([
-#         [boton1]
-#         ])
-#     )
+
+# if __name__ == '__main__':
+#     updater = Updater(token='5772798648:AAELl54LNmwV2bg12CR8ugiyTDln_l2aLsE', use_context=True)   
+
+#     dp = updater.dispatcher   
+#     dp.add_handler(MessageHandler(filters=Filters.all, callback='start'))  
+
+#     updater.start_polling()
+#     print('El bot se inicio')
+#     updater.idle()
+
+
+
+
+# def process_message(update, context):
+#     text = update.message.text
+
+#     if str(text).__contains__('#channel'):
+#         context.bot.send_message(
+#             chat_id='-1001734611459',
+#             text=str(text).replace('#channel', '')
+#         )
+
+#     print(str(text))
